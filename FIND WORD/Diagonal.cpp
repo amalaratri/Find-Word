2acd968a5e0922380cdd026a74dca412e3a1e845
Nulnul <151645826+HusnulKhotami@users.noreply.github.com>
2024-04-27 23:56:42 +0700

    Update Diagonal.cpp

--- FIND WORD/Diagonal.cpp
+++ FIND WORD/Diagonal.cpp
@@ -1,30 +1,71 @@
-bool diagonal(const char* word, char matrix[][24], int wordLen) {
-    for (int i = 0; i < 24; i++) {
-        for (int j = 0; j < 24; ++j) {
+bool diagonalUpLeft(const char* word, char matrix[][24], int wordLen) {
+    for (int i = 0; i <= 24 - wordLen; i++) {
+        for (int j = 0; j <= 24 - wordLen; j++) {
             bool found = true;
-            // Untuk mengecek diagonal kanan bawah
-            if (i <= 24 - wordLen && j <= 24 - wordLen) {
-                for (int k = 0; k < wordLen; k++) {
-                    if (matrix[i + k][j + k] != word[k]) {
-                        found = false;
-                        break;
-                    }
+            for (int k = 0; k < wordLen; k++) {
+                if (matrix[i + k][j + k] != word[k]) {
+                    found = false;
+                    break;
                 }
-                if (found) return true;
             }
-            // Mengcek diagonal kiri bawah
-            found = true;
-            if (i <= 24 - wordLen && j >= wordLen - 1) {
-                for (int k = 0; k < wordLen; k++) {
-                    if (matrix[i + k][j - k] != word[k]) {
-                        found = false;
-                        break;
-                    }
-                }
-                if (found) return true;
+            if (found) {
+                return true;
             }
         }
     }
     return false;
 }
 
+bool diagonalUpRight(const char* word, char matrix[][24], int wordLen) {
+    for (int i = 0; i <= 24 - wordLen; i++) {
+        for (int j = wordLen - 1; j < 24; j++) {
+            bool found = true;
+            for (int k = 0; k < wordLen; k++) {
+                if (matrix[i + k][j - k] != word[k]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool diagonalDownLeft(const char* word, char matrix[][24], int wordLen) {
+    for (int i = wordLen - 1; i < 24; i++) {
+        for (int j = 0; j <= 24 - wordLen; j++) {
+            bool found = true;
+            for (int k = 0; k < wordLen; k++) {
+                if (matrix[i - k][j + k] != word[k]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool diagonalDownRight(const char* word, char matrix[][24], int wordLen) {
+    for (int i = wordLen - 1; i < 24; i++) {
+        for (int j = wordLen - 1; j < 24; j++) {
+            bool found = true;
+            for (int k = 0; k < wordLen; k++) {
+                if (matrix[i - k][j - k] != word[k]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
