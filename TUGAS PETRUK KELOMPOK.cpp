afd4e253c0f6968c80001d564ca992888341713a
HusnulKhotami <khotamihusnul@gmail.com>
2024-04-27 20:10:29 +0700

    menambahkan fungsi diagonal untuk mencari kata didalam fungsi matrix

--- TUGAS PETRUK KELOMPOK.cpp
+++ TUGAS PETRUK KELOMPOK.cpp
@@ -40,5 +40,35 @@ bool vertical(const char* word, char matrix[][24], int wordLen) {
     return false;
 }
 
+bool diagonal(const char* word, char matrix[][24], int wordLen) {
+    for (int i = 0; i < 24; i++) {
+        for (int j = 0; j < 24; ++j) {
+            bool found = true;
+            
+            if (i <= 24 - wordLen && j <= 24 - wordLen) {
+                for (int k = 0; k < wordLen; k++) {
+                    if (matrix[i + k][j + k] != word[k]) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) return true;
+            }
+            
+            found = true;
+            if (i <= 24 - wordLen && j >= wordLen - 1) {
+                for (int k = 0; k < wordLen; k++) {
+                    if (matrix[i + k][j - k] != word[k]) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) return true;
+            }
+        }
+    }
+    return false;
+}
+
 
   
