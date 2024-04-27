c293e035f554271165f4da6aec5d3ec77db511d0
fitrianuraini <fitrianuraini211@gmail.com>
2024-04-27 22:44:34 +0700

    Mencari kata secara diagonal dari kanan dan kiri atas

--- TUGAS PETRUK KELOMPOK.cpp
+++ TUGAS PETRUK KELOMPOK.cpp
@@ -40,30 +40,36 @@ bool vertical(const char* word, char matrix[][23], int wordLen) {
     return false;
 }
 
-bool diagonal(const char* word, char matrix[][23], int wordLen) {
-    for (int i = 0; i < 23; i++) {
-        for (int j = 0; j < 23; ++j) {
+bool diagonalUpLeft(const char* word, char matrix[][24], int wordLen) {
+    for (int i = 0; i <= 24 - wordLen; i++) {
+        for (int j = 0; j <= 24 - wordLen; j++) {
             bool found = true;
-            
-            if (i <= 23 - wordLen && j <= 23 - wordLen) {
-                for (int k = 0; k < wordLen; k++) {
-                    if (matrix[i + k][j + k] != word[k]) {
-                        found = false;
-                        break;
-                    }
+            for (int k = 0; k < wordLen; k++) {
+                if (matrix[i + k][j + k] != word[k]) {
+                    found = false;
+                    break;
                 }
-                if (found) return true;
             }
-            
-            found = true;
-            if (i <= 23 - wordLen && j >= wordLen - 1) {
-                for (int k = 0; k < wordLen; k++) {
-                    if (matrix[i + k][j - k] != word[k]) {
-                        found = false;
-                        break;
-                    }
+            if (found) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool diagonalUpRight(const char* word, char matrix[][24], int wordLen) {
+    for (int i = 0; i <= 24 - wordLen; i++) {
+        for (int j = wordLen - 1; j < 24; j++) {
+            bool found = true;
+            for (int k = 0; k < wordLen; k++) {
+                if (matrix[i + k][j - k] != word[k]) {
+                    found = false;
+                    break;
                 }
-                if (found) return true;
+            }
+            if (found) {
+                return true;
             }
         }
     }
