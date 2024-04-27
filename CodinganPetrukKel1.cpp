656635c33ee0b67d3830d58293b4e80f9d656b33
fitrianuraini <fitrianuraini211@gmail.com>
2024-04-27 15:22:51 +0700

    Mencari Kata Secara diagonal dan matriks

--- CodinganPetrukKel1.cpp
+++ CodinganPetrukKel1.cpp
@@ -47,7 +47,7 @@ bool diagonal(const char* word, char matrix[][24], int wordLen) {
     for (int i = 0; i < 24; ++i) {
         for (int j = 0; j < 24; ++j) {
             bool found = true;
-            // Cek diagonal kanan bawah
+            // untuk cek diagonal kanan bawah
             if (i <= 24 - wordLen && j <= 24 - wordLen) {
                 for (int k = 0; k < wordLen; ++k) {
                     if (matrix[i + k][j + k] != word[k]) {
